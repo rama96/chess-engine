5aa60d77ba471486c2698aec16740c5bf12cf91a
Ramamurthi <ramamurthi@192.168.1.6>
2023-10-22 01:09:31 +0530

    adding make move method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , request ,  jsonify
 
 app = Flask(__name__)
 
@@ -7,5 +7,19 @@ def index():
     initial_position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
     return render_template('index.html', initial_position=initial_position)
 
+@app.route('/make_move', methods=['POST'])
+def make_move():
+    data = request.get_json()
+    move = data.get('move')
+
+    # Process the move and update the game state
+    # Example: game.make_move(move)
+
+    # Return a response to the client
+    print(move)
+    response_data = {'message': 'Move successful'}
+    return jsonify(response_data)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8081)
