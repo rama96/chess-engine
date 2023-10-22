554c7d7f49d21310537c04c5d5da791e7d8c8958
Ramamurthi <ramamurthi@192.168.1.6>
2023-10-22 15:09:27 +0530

    enabling notations

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var board = ChessBoard('board', {
         position: 'start',
         draggable: true,
+        showNotation: true,
         pieceTheme: '/static/img/chesspieces/wikipedia/{piece}.png',
         onDrop: function (source, target, piece, newPos, oldPos, orientation) {
             var move = source + '-' + target;
