f71d4c007d30485795f4ce510eeb3b2ddec2b87f
Ramamurthi <ramamurthi@192.168.1.6>
2023-10-22 14:58:17 +0530

    Prevents default submission

--- static/js/main.js
+++ static/js/main.js
@@ -1,25 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var board = ChessBoard('board', {
-        position: 'start',
-        draggable: true,
-        pieceTheme: '/static/img/chesspieces/wikipedia/{piece}.png',
-        onDrop: function (source, target, piece, newPos, oldPos, orientation) {
-            var move = source + '-' + target;
+(function() {
+    document.addEventListener('DOMContentLoaded', function() {
+        var board = ChessBoard('board', {
+            position: 'start',
+            draggable: true,
+            pieceTheme: '/static/img/chesspieces/wikipedia/{piece}.png',
+            onDrop: function (source, target, piece, newPos, oldPos, orientation) {
+                var move = source + '-' + target;
 
-            fetch('/make_move', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ move: move }),
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-        }
+                fetch('/make_move', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ move: move }),
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+            return true;
+            }
+        });
+
+        var chessboardForm = document.getElementById('chessboard-form');
+
+        chessboardForm.addEventListener('submit', function(event) {
+            event.preventDefault(); // Prevent the default form submission
+        });
     });
-});
+})();
